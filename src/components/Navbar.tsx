46a4bc1aeb7dd825090007f05f7a1452a6141364
Brandon-Schefstad <bschefstad@gmail.com>
2022-12-17 00:58:32 -0500

    Redesigning

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,17 +17,17 @@ const Navbar = () => {
 					</li> */}
 			<div className="btm-nav bg-black z-30  glass fixed bottom-0 left-0 col-span-4 px-4 pt-[-1rem]xl:pt-4 border-2 border-white  border-solid grid grid-cols-3 gap-8 ">
 				<a href="#Nature">
-					<div className="nav-item text-sm btn w-1/2  hover:text-white  rounded-none hover:bg-black bg-slate-200 text-black">
+					<div className="nav-item text-sm btn xl:w-1/2  hover:text-white  rounded-none hover:bg-black bg-slate-200 text-black">
 						Nature
 					</div>
 				</a>
 				<a href="#Auto">
-					<div className="nav-item text-sm btn w-1/2  hover:text-white  rounded-none hover:bg-black bg-slate-200 text-black">
+					<div className="nav-item text-sm btn xl:w-1/2  hover:text-white  rounded-none hover:bg-black bg-slate-200 text-black">
 						Auto
 					</div>
 				</a>
 				<a href="#Family">
-					<div className="nav-item text-sm btn w-1/2  hover:text-white  rounded-none hover:bg-black bg-slate-200 text-black">
+					<div className="nav-item text-sm btn xl:w-1/2  hover:text-white  rounded-none hover:bg-black bg-slate-200 text-black">
 						Family
 					</div>
 				</a>
