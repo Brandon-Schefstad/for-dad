9c8b6b519b074afb5c67d259120c3a60bfc44e8a
Brandon-Schefstad <bschefstad@gmail.com>
2022-12-17 00:33:36 -0500

    Adding nvmrc

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -4,7 +4,7 @@ const SectionTitle = ({ title, classList }) => {
 	return (
 		<div
 			className={`${classList} m-auto text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-center col-span-7 `}>
-			<h3 className="slider left">{title}</h3>
+			<h3 className="slider left ">{title}</h3>
 		</div>
 	)
 }
